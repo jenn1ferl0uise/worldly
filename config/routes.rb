09837e832133fa9d674c47dc47c79cccfa7863be
Jennifer Lynch <jl.lynch9@gmail.com>
2019-04-06 00:08:01 +0200

    rooted to index to view map

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  get 'places/info'
+  root to: 'places#index'
+  get 'info', to: 'pages#info', as: 'info'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
