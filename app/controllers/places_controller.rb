bbba77c8571987aa9f21d347e7b42601797df1fa
Jennifer Lynch <jl.lynch9@gmail.com>
2019-04-05 23:36:40 +0200

    added div with map to index view

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,4 +2,13 @@ class PlacesController < ApplicationController
   def info
     @place = Place.where(id: @id)
   end
+  def index
+    @places = Place.where.not(latitude: nil, longitude: nil)
+    @markers = @places.map do |place|
+      {
+        lat: place.latitude,
+        lng: place.longitude
+      }
+    end
+  end
 end
