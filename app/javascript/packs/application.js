a5ea28b758a49a493a7eb86e5b88596cffa4c5de
Jennifer Lynch <jl.lynch9@gmail.com>
2019-04-05 23:45:10 +0200

    added mapbox javascript

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,3 +16,8 @@
 // const imagePath = (name) => images(name, true)
 
 console.log('Hello World from Webpacker')
+import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
+
+import { initMapbox } from '../plugins/init_mapbox';
+
+initMapbox();
